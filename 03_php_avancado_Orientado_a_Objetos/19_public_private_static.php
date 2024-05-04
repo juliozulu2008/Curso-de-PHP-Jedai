7a6f39aee652b0a3584f4b3b5a8577ffef256b34
Julio Cesar <juliozulu2008@gmail.com>
2024-05-04 17:32:51 -0300

    Public Private e static

--- 03_php_avancado_Orientado_a_Objetos/19_public_private_static.php
+++ 03_php_avancado_Orientado_a_Objetos/19_public_private_static.php
@@ -1,2 +1,10 @@
 <?php 
 
+include("Exemplo.class.php");
+//instancia de um objeto!
+$exemplo = new Exemplo();
+// se deixar passar erro fatal
+//$exemplo->var1 = "Ola"; IDE nao reconhece isso pois esta private
+$exemplo->var2 = "Mundo";
+//variável estática
+echo Exemplo::$var3;  // Saída: Estática
---
+++ 03_php_avancado_Orientado_a_Objetos/Exemplo.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class Exemplo
+{
+    private $var1;
+    public $var2;
+    public static $var3 = "Este é uma variável estática";
+
+    public function metodo(){
+
+    }
+    private function metodoPrivado(){
+    }
+}
